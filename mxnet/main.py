41374787be80fd5eb6b139e45a2a0c6449408fe4
Data-drone <bpl.law@gmail.com>
2021-02-27 22:44:10 +1100

    corrected and got mxnet example to run properly

--- mxnet/main.py
+++ mxnet/main.py
@@ -10,14 +10,17 @@
 #### Installing Required Libraries
 ####
 
-!pip3 install https://repo.mxnet.io/dist/python/cu110/mxnet_cu110-2.0.0b20210223-py3-none-manylinux2014_x86_64.whl
+!pip3 install mxnet-cu110==1.9.0b20210226 -f https://dist.mxnet.io/python/cu110
 !pip3 install scikit-learn pandas
 
 # Load Libraries
 import mxnet as mx
-from mxnet import nd
+from mxnet import nd, autograd
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn.metrics as metrics
+from sklearn.metrics import confusion_matrix
+
 
 
 # Check if CUDA is loaded properly
@@ -35,9 +38,10 @@ ctx = mx.gpu()
 
 # define the transformations
 # in Gluon we transform first
-def transformations(data, labels):
+def transformations(data, label):
     data = mx.nd.transpose(data, (2,0,1))
     data = data.astype(np.float32)
+    label = label.astype(np.float32)
     return data, label
 
 # Load data from the Gluon default data libraries
@@ -132,8 +136,8 @@ for e in range(epochs):
 
         count +=1
 
-    test_accuracy = evaluate_accuracy(test_data, alex_net)
-    train_accuracy = evaluate_accuracy(train_data, alex_net)
+    test_accuracy = evaluate_accuracy(test_data, simple_network)
+    train_accuracy = evaluate_accuracy(train_data, simple_network)
     print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
 
     epoch_list.append(e)
@@ -148,5 +152,23 @@ plt.plot(iteration_list, loss_list)
 
 #### Visualise the training accuracy
 plt.plot(epoch_list, accuracy_list)
+
+### Confusion Matrix Report
+labels_list = []
+output_list = []
+
+for i, batch in enumerate(test_data):
+    data = mx.gluon.utils.split_and_load(batch[0], ctx_list=[ctx], batch_axis=0)
+    label = mx.gluon.utils.split_and_load(batch[1], ctx_list=[ctx], batch_axis=0)
+    outputs = [simple_network(x) for x in data]
+    
+    for l in label:
+        labels_list.extend(l.asnumpy().tolist())
+    for o in outputs[0]:
+        output_list.append(np.argmax(o.asnumpy()))    
+
+confusion_matrix(labels_list, output_list)
+print("Classification report for CNN :\n%s\n"
+      % (metrics.classification_report(labels_list, output_list)))
 
 

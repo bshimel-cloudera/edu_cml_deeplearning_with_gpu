ea85051f464cfac078a0d6badead1ac9ce08599e
Jeff Fletcher <fletch.jeff@gmail.com>
2021-04-20 12:35:19 +0200

    Implemented the tensorflow cuda fix

--- tensorflow/main.py
+++ tensorflow/main.py
@@ -13,6 +13,10 @@
 !pip install tensorflow --progress-bar off
 !pip install scikit-learn pandas --progress-bar off
 
+####
+#### The follow line is required to fix an issue with the latest Cuda drivers and tensorflow.
+####
+!ln -s /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.11.0.1.105 /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.10
 
 
 # import libraries

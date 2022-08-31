0db366063f486c59cdcc70e4e2d4c9d2e3bd4f4e
bshimel-cloudera <106165837+bshimel-cloudera@users.noreply.github.com>
2022-08-31 16:40:05 -0500

    Remove environment setup

--- tensorflow/main.py
+++ tensorflow/main.py
@@ -2,23 +2,6 @@
 # Main Tensorflow Code Loop
 #
 
-########
-########  Setup Environment and install libs
-########
-
-
-####
-#### Installing Required Libraries
-####
-!pip install tensorflow --progress-bar off
-!pip install scikit-learn pandas --progress-bar off
-
-####
-#### The follow line is required to fix an issue with the latest Cuda drivers and tensorflow.
-####
-!ln -s /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.11.0.1.105 /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.10
-
-
 # import libraries
 import tensorflow as tf
 import numpy as np

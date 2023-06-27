03c65cd2898c839c73a95b1e7f9bfcc039333748
bshimel-cloudera <106165837+bshimel-cloudera@users.noreply.github.com>
2023-06-27 15:21:51 -0500

    Update environment_setup.py to work with Runtime 2023.05

--- tensorflow/environment_setup.py
+++ tensorflow/environment_setup.py
@@ -2,14 +2,20 @@
 ########  Setup Environment and install libs
 ########
 
+# This version tested with Runtime:
+#  Editor: Workbench
+#  Kernel: Python 3.7
+#  Edition: Nvidia GPU
+#  Version: 2023.05f
 
 ####
 #### Installing Required Libraries
 ####
-!pip install tensorflow --progress-bar off
-!pip install scikit-learn pandas --progress-bar off
+!pip install tensorflow==2.9.1 --progress-bar off
+!pip install scikit-learn==1.0.2 pandas==1.3.5 --progress-bar off
 
 ####
 #### The follow line is required to fix an issue with the latest Cuda drivers and tensorflow.
 ####
-!ln -s /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.11.0.1.105 /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.10
+!ln -s /usr/local/cuda-11.4/targets/x86_64-linux/lib/libcusolver.so.11.2.0.120 /usr/local/cuda-11.4/targets/x86_64-linux/lib/libcusolver.so.10
+!echo export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.4/compat:/usr/local/cuda-11.4/lib64 >>.bash_profile
